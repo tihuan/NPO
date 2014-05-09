0e3c127d8f9ebf51f2e1048587c98b83b00571b1
Timmy Huang <TimmyHuang0404@Gmail.com>
2014-05-09 16:45:28 -0400

    show_supply_qty

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,5 +2,8 @@
 
 # Homepage
 get "/" do
+  event = Event.first
+  @aq_supplies = event.supplies
+  @nd_supplies = EventSupply.where(event_id: event.id)
   erb :index
 end
--- app/helpers/formatting.rb
+++ app/helpers/formatting.rb
@@ -2,4 +2,8 @@ helpers do
   def em(text)
     "<em>#{text}</em>"
   end
+
+  def supply_name(supply_id)
+    Supply.find(supply_id).name
+  end
 end
